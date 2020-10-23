43b9ac9fe07ff07192b3e3bcf3f7bcbe68d5df26
Ákos Uzonyi <uzonyi.akos@gmail.com>
2020-10-23 13:39:27 +0200

    add CalendarFragment

--- android/app/src/main/java/com/tisza/esemenynaptar/MainActivity.kt
+++ android/app/src/main/java/com/tisza/esemenynaptar/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.tisza.esemenynaptar
 
 import android.app.*
-import android.app.DatePickerDialog.*
-import android.app.TimePickerDialog.*
 import android.content.*
 import android.os.*
 import android.view.*
 import androidx.appcompat.app.*
 import androidx.appcompat.widget.*
-import androidx.viewpager.widget.*
-import androidx.viewpager.widget.ViewPager.*
 import com.tisza.esemenynaptar.database.*
 import java.text.*
 import java.util.*
@@ -20,26 +16,10 @@ const val SHARED_PREF = "sp"
 const val SP_NOTIFICATIONS_ENABLED = "notifications_enabled"
 const val SP_NOTIFICATION_TIME = "notification_time"
 const val TODAY_EXTRA = "today"
-private const val SAVED_DATE = "date"
-private const val SAVING_DAY = "savingdate"
 
-class MainActivity : AppCompatActivity(), OnPageChangeListener {
-    private lateinit var pager: ViewPager
-    private lateinit var pagerAdapter: MyPagerAdapter
+class MainActivity : AppCompatActivity() {
     private lateinit var inflater: LayoutInflater
     private lateinit var sharedPreferences: SharedPreferences
-    private lateinit var savedDate: Calendar
-
-    private val dateSetListener = OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-        pagerAdapter.setDate(year, monthOfYear, dayOfMonth)
-    }
-
-    private val nofificationTimeSetListener = OnTimeSetListener { view, hourOfDay, minute ->
-        val editor = sharedPreferences.edit()
-        editor.putInt(SP_NOTIFICATION_TIME, hourOfDay * 60 + minute)
-        editor.commit()
-        scheduleNotifications(this@MainActivity)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,10 +29,6 @@ class MainActivity : AppCompatActivity(), OnPageChangeListener {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-        savedDate = Calendar.getInstance()
-        val today = savedDate.timeInMillis / MILLIS_PER_DAY
-        if (savedInstanceState != null && savedInstanceState.getLong(SAVING_DAY) == today)
-            savedDate.timeInMillis = savedInstanceState.getLong(SAVED_DATE)
         createNotificationChannel(this)
         scheduleNotifications(this)
         val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -60,25 +36,7 @@ class MainActivity : AppCompatActivity(), OnPageChangeListener {
     }
 
     private fun onDatabaseReady() {
-        pager = findViewById<View>(R.id.pager) as ViewPager
-        pager.setOnPageChangeListener(this)
-        pagerAdapter = MyPagerAdapter(this, pager)
-        pager.adapter = pagerAdapter
-        pagerAdapter.date = savedDate
-    }
-
-    override fun onSaveInstanceState(bundle: Bundle) {
-        super.onSaveInstanceState(bundle)
-        val now = Calendar.getInstance()
-        bundle.putLong(SAVED_DATE, pagerAdapter.date.timeInMillis)
-        bundle.putLong(SAVING_DAY, now.timeInMillis / MILLIS_PER_DAY)
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        if (intent.getBooleanExtra(TODAY_EXTRA, false)) {
-            pagerAdapter.date = Calendar.getInstance()
-        }
+        supportFragmentManager.beginTransaction().replace(R.id.main_frame, CalendarFragment()).commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -87,53 +45,10 @@ class MainActivity : AppCompatActivity(), OnPageChangeListener {
         return true
     }
 
-    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
-        val setTimeItem = menu.findItem(R.id.set_notification_time)
-        val enableNotificationItem = menu.findItem(R.id.enable_notification)
-        setTimeItem.isEnabled = enableNotificationItem.isChecked
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.today -> {
-                pagerAdapter.date = Calendar.getInstance()
-                true
-            }
-            R.id.pick_date -> {
-                val date = pagerAdapter.date
-                val dateDialog = DatePickerDialog(this, dateSetListener, date[Calendar.YEAR], date[Calendar.MONTH], date[Calendar.DAY_OF_MONTH])
-                dateDialog.show()
-                true
-            }
-            R.id.enable_notification -> {
-                val enable = !item.isChecked
-                item.isChecked = enable
-                val editor = sharedPreferences.edit()
-                editor.putBoolean(SP_NOTIFICATIONS_ENABLED, enable)
-                editor.commit()
-                supportInvalidateOptionsMenu()
-                true
-            }
-            R.id.set_notification_time -> {
-                val time = sharedPreferences.getInt(SP_NOTIFICATION_TIME, 420)
-                val timeDialog = TimePickerDialog(this, nofificationTimeSetListener, time / 60, time % 60, true)
-                timeDialog.show()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        if (intent.getBooleanExtra(TODAY_EXTRA, false)) {
+            //pagerAdapter.date = Calendar.getInstance()
         }
-    }
-
-    override fun onPageScrollStateChanged(state: Int) {
-        pagerAdapter.onPageScrollStateChanged(state)
-    }
-
-    override fun onPageScrolled(arg0: Int, arg1: Float, arg2: Int) {
-        pagerAdapter.onPageScrolled(arg0, arg1, arg2)
-    }
-
-    override fun onPageSelected(page: Int) {
-        pagerAdapter.onPageSelected(page)
     }
 }
